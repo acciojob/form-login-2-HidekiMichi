2a82ef20ed5f3a37fcd24bdbb487e6c9de10e29d
HidekiMichi <105813974+HidekiMichi@users.noreply.github.com>
2024-01-31 15:05:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let label1 = document.querySelector('label[for="First Name"]');
+let label2 = document.querySelector('label[for="Last Name"]');
+let label3 = document.querySelector('label[for="Phone Number"]');
+let label4 = document.querySelector('label[for="Email ID"]');
+
+let fullName = document.querySelector('input[name="First Name"]');
+let lastName = document.querySelector('input[name="Last Name"]');
+let Phone = document.querySelector('input[name="Phone Number"]');
+let Email = document.querySelector('input[name="Email ID"]');
+
+function submitDetails() {
+    alert(`${label1.textContent} ${fullName.value}
+${label2.textContent} ${lastName.value}
+${label3.textContent} ${Phone.value}
+${label4.textContent} ${Email.value}
+    `);
+}
